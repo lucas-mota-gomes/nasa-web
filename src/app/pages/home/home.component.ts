e3b2228df3c5b7dfa3ae6265c1dc684af50bb1aa
Lucas Mota <lcasmotagomes@hotmail.com>
2024-10-05 22:35:57 -0300

    Refactor home component: Update home page layout and add search functionality

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,19 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { TableModule } from 'primeng/table';
 import { FormsModule } from '@angular/forms';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
 import { ButtonModule } from 'primeng/button';
+import { AutoCompleteModule } from 'primeng/autocomplete';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TableModule, FormsModule, ProgressSpinnerModule, ButtonModule],
+  imports: [TableModule, FormsModule, ProgressSpinnerModule, ButtonModule, AutoCompleteModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
+  encapsulation: ViewEncapsulation.None
 })
 export class HomeComponent implements OnInit {
 
   constructor() { }
+
+  items: any[] | undefined;
+
+  selectedItem: any;
+
+  suggestions: any;
+
+  search(event: any) {
+      this.suggestions = [...Array(10).keys()].map(item => event.query + '-' + item);
+  }
 
   ngOnInit(): void {
 
